113390705476070d350bbbf21f454c5a3895bb31
theomorphic <FelixMK@yandex.ru>
2025-01-07 15:00:42 +0500

    feat: add home buildings and fix some bugs

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,30 @@
+
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+	<div className="home__socials">
+		<a href="https://www.instagram.com/mishakai3er" className="home__social-link" target='_blank' rel='noreferrer'>
+			<i class="fa-brands fa-instagram"></i>
+		</a>
+
+		<a href="https://x.com/mishakai3er" className="home__social-link" target='_blank' rel='noreferrer'>
+			<i class="fa-brands fa-twitter"></i>
+		</a>
+
+		<a href="https://vk.com/mishakai3er" className="home__social-link" target='_blank' rel='noreferrer'>
+			<i class="fa-brands fa-vk"></i>
+		</a>
+
+		<a href="https://www.facebook.com/profile.php?id=100010803093074" className="home__social-link" target='_blank' rel='noreferrer'>
+			<i class="fa-brands fa-facebook"></i>
+		</a>
+
+		<a href="https://github.com/theomorphic" className="home__social-link" target='_blank' rel='noreferrer'>
+			<i class="fa-brands fa-github"></i>
+		</a>
+	</div>
+  )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,26 @@
 
 import React from "react";
 import "./home.css"
+import Me from "../../assets/avatar-1.svg"
+import HeaderSocials from "./HeaderSocials";
+import ScrollDown from "./ScrollDown";
 
 const Home = ()=>{
 	return(
-		<div>Home</div>
+		<section className="home container" id="home">
+			<div className="intro">
+				<img src={Me} alt="Misha Kaiser" className="home__img" />
+				<h1 className="home__name">Misha Kaiser</h1>
+				<span className="home__education">React developer</span>
+
+				<HeaderSocials />
+
+				<a href="#contact" className="btn">Contact Me</a>
+			
+				<ScrollDown />
+			
+			</div>
+		</section>
 	)
 }
 
